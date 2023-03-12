f3f0337092927a2885fc4187183993e998e54627
Sea <yasuo2712002@gmail.com>
2023-03-12 17:03:24 +0700

    update king

--- config/socket.js
+++
@@ -1,8 +0,0 @@
-const { Server } = require("socket.io");
-
-module.exports = function(server) {
-  const io = new Server(server);
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
-}
