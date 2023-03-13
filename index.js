c586f3eb573b927a6c1c18273aaf0528fecb1586
Sea <yasuo2712002@gmail.com>
2023-03-13 19:44:28 +0700

    upadte

--- index.js
+++ index.js
@@ -36,11 +36,12 @@ var walletRouter = require('./routes/wallet');
 var app = express();
 const server = http.createServer(app);
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
-
-
+app.get('/', function(req, res){
+  res.render("index");
+});
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
